123d53dc8e3193fcaf9772aa0cd802f67b8a3ed3
AEU <aeuplayground@gmail.com>
2025-06-18 22:59:15 +0900

    feature : 프로젝트 옮기기

--- src/component/three/ShoppingCanvas.tsx
+++ src/component/three/ShoppingCanvas.tsx
@@ -1,5 +1,25 @@
-function ShoppingCanvas() {
-  return <></>;
+import { Canvas } from "@react-three/fiber";
+import { css } from "@emotion/react";
+import ShoesMesh from "./ShoesMesh";
+import { OrbitControls } from "@react-three/drei";
+
+function ShoppingCanvas({ color }: { color: string }) {
+  return (
+    <Canvas
+      css={css({
+        width: "100%",
+        height: "100%",
+      })}
+    >
+      <OrbitControls />
+      <axesHelper />
+      <gridHelper args={[10, 10]} />
+
+      <ambientLight />
+      <directionalLight />
+      <ShoesMesh color={color} />
+    </Canvas>
+  );
 }
 
 export default ShoppingCanvas;
